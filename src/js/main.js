6675c24a6162197b6f99285db21b47a6d421b9a2
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-31 00:13:45 +0200

    Refactoring navigation's buttons

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// Elements of DOM
+
+const $subscription = document.querySelector("#subscription");
+const $connection = document.querySelector("#connection");
+const $disconnection = document.querySelector("#disconnection");
