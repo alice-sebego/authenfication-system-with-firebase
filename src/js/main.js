cf9027515522e4c04bea87565239c3745e7b43d3
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-01 01:19:29 +0200

    Switch between subscription's form and connection's form

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,30 @@
 const $subscription = document.querySelector("#subscription");
 const $connection = document.querySelector("#connection");
 const $disconnection = document.querySelector("#disconnection");
+const $subscribe = document.querySelector("#subscribe");
+const $connect = document.querySelector("#connect");
+const $picture = document.querySelector("#picture");
+
+$subscription.addEventListener("click", ()=> {
+    if($connect.classList.contains("redisplay")){
+        $connect.classList.replace("redisplay", "notdisplayed");
+        $subscribe.classList.replace("notdisplayed", "redisplay");
+        $picture.classList.replace("notdisplayed", "redisplay");
+    } else{
+        $subscribe.classList.replace("notdisplayed", "redisplay");
+        $picture.classList.replace("notdisplayed", "redisplay");
+    }
+
+});
+
+$connection.addEventListener("click", ()=> {
+    if($subscribe.classList.contains("redisplay")){
+        $subscribe.classList.replace("redisplay", "notdisplayed");
+        $connect.classList.replace("notdisplayed", "redisplay");
+        $picture.classList.replace("notdisplayed", "redisplay");
+    } else {
+        $connect.classList.replace("notdisplayed", "redisplay");
+        $picture.classList.replace("notdisplayed", "redisplay");
+    }
+
+});
