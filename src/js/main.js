a00ab0492c35cbfffc517781c59fa39d2fa9ac63
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-01 01:32:18 +0200

    Create regex for validation's inputs

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,29 @@
-// Elements of DOM
+import * as regex from './regex';
 
+// Elements of DOM
 const $subscription = document.querySelector("#subscription");
 const $connection = document.querySelector("#connection");
 const $disconnection = document.querySelector("#disconnection");
+const $h2 = document.querySelector("h2");
 const $subscribe = document.querySelector("#subscribe");
 const $connect = document.querySelector("#connect");
 const $picture = document.querySelector("#picture");
 
-$subscription.addEventListener("click", ()=> {
+$subscription.addEventListener("click", () => {
+
     if($connect.classList.contains("redisplay")){
         $connect.classList.replace("redisplay", "notdisplayed");
         $subscribe.classList.replace("notdisplayed", "redisplay");
         $picture.classList.replace("notdisplayed", "redisplay");
-    } else{
+    } else {
         $subscribe.classList.replace("notdisplayed", "redisplay");
         $picture.classList.replace("notdisplayed", "redisplay");
     }
 
 });
 
-$connection.addEventListener("click", ()=> {
+$connection.addEventListener("click", () => {
+
     if($subscribe.classList.contains("redisplay")){
         $subscribe.classList.replace("redisplay", "notdisplayed");
         $connect.classList.replace("notdisplayed", "redisplay");
---
+++ src/js/regex.js
@@ -0,0 +1,5 @@
+export const email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+export const passwordLength = /^(.{8,})/;
+export const digitPresence = /^(?=.*\d)/;
+export const lowcasePresence = /^(?=.*[a-z])/;
+export const uppercasePresence = /^(?=.*[A-Z])/;
