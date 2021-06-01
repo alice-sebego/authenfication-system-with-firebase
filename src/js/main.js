ee9277cc80105afd61c27f4adb5f92ab5672ffb9
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-01 22:53:59 +0200

    Complete the Validation class to manage the entire input's events

--- src/js/main.js
+++ src/js/main.js
@@ -10,17 +10,21 @@ const $connect = document.querySelector("#connect");
 const $picture = document.querySelector("#picture");
 // Forms elements
 const $fieldEmailsub = document.querySelector("#fieldEmailsub");
-const $fieldPwdsub = document.querySelector("#fieldPwdsub");
+const $fieldPwdSub = document.querySelector("#fieldPwdSub");
 const $emailSubscription = document.querySelector("#emailSubscription");
 const $pwdSubscription = document.querySelector("#pwdSubscription");
+const $infoMailSub = document.querySelector("#infoMailSub");
+const $infoPwdSub = document.querySelector("#infoPwdSub");
 
 const $fieldEmailConnect = document.querySelector("#fieldEmailConnect");
 const $fieldPwdConnect = document.querySelector("#fieldPwdConnect");
 const $emailConnection = document.querySelector("#emailConnection");
 const $pwdConnection = document.querySelector("#pwdConnection");
+const $infoMailConx = document.querySelector("infoMailConx");
+const $infoPwdConx = document.querySelector("#infoPwdConx");
 
 
-
+// Listening all navigation's buttons
 $subscription.addEventListener("click", () => {
 
     if($connect.classList.contains("redisplay")){
@@ -48,11 +52,11 @@ $connection.addEventListener("click", () => {
 });
 
 // Validate input's user who is subscribing
-const validateSubscription = new Validation($fieldEmailsub, $fieldPwdsub, $emailSubscription, $pwdSubscription);
+const validateSubscription = new Validation($fieldEmailsub, $fieldPwdSub, $emailSubscription, $pwdSubscription, $infoMailSub, $infoPwdSub);
 validateSubscription.validateEmail();
 validateSubscription.vadidatePassword();
 
 // Validate input's user who is connecting
-const validateConnection = new Validation($fieldEmailConnect, $fieldPwdConnect, $emailConnection, $pwdConnection);
+const validateConnection = new Validation($fieldEmailConnect, $fieldPwdConnect, $emailConnection, $pwdConnection, $infoMailConx, $infoPwdSub);
 validateConnection.validateEmail();
 validateConnection.vadidatePassword();
--- src/js/validation-input.js
+++ src/js/validation-input.js
@@ -2,34 +2,49 @@ import * as regex from './regex.js';
 
 export default class Validation{
     
-    constructor(fieldsetEmail, fiedlsetPwd, email, password){
+    constructor(fieldsetEmail, fieldsetPwd, email, password, infoMail, infoPwd){
         this.fieldsetEmail = fieldsetEmail;
-        this.fiedlsetPwd = fiedlsetPwd;
+        this.fieldsetPwd = fieldsetPwd;
         this.email = email;
         this.password = password;
+        this.infoMail = infoMail;
+        this.infoPwd = infoPwd;
     }
 
     validateEmail(){
-        this.email.addEventListener("input", ()=>{
+        this.email.addEventListener("input", () =>{
+
             if(regex.email.test(this.email.value)){
-                console.log("ok");    
+                console.log("ok");
+                this.infoMail.classList.add("right");
+                this.infoMail.innerHTML = `Votre adresse e-mail est valide`;
+
             } else {
                 console.log("votre email n'est pas valide");
+                this.infoMail.classList.add("wrong");
+                this.infoMail.innerHTML = `Votre adresse e-mail n'est pas valide`;
             }
         })
     }
 
     vadidatePassword(){
-        this.password.addEventListener("input", ()=>{
+        this.password.addEventListener("input", () => {
+
             if(regex.passwordLength.test(this.password.value) &&
                regex.digitPresence.test(this.password.value) &&
                regex.lowcasePresence.test(this.password.value) &&
                regex.uppercasePresence.test(this.password.value)
             ){
-                console.log("ok");    
+                console.log("ok");
+                this.infoPwd.classList.add("right");
+                this.infoPwd.innerHTML = `Votre mot de passe est valide`;
+
             } else {
-                console.log("votre mdp doit contenir au moins 1 minuscule, 1 majuscule, 1 chiffre et avoir 8 caractères");
+                console.log("Votre mdp doit contenir au moins 1 minuscule, 1 majuscule, 1 chiffre et avoir 8 caractères");
+                this.infoPwd.classList.add("wrong");
+                this.infoPwd.innerHTML = `Votre mdp doit contenir au moins 1 minuscule, 1 majuscule, 1 chiffre et avoir 8 caractères`;
             }
         })
     }
+
 }
